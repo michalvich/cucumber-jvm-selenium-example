9bc8ae62c7f213fa844c655592db9642f0391b6f
Michal Vich <michal.vich@gmail.com>
2013-02-01 16:04:33 +0100

    fixed feature and definition

--- src/test/java/com/michalvich/cucumber/selenium/example/GoogleSearchScenario.java
+++ src/test/java/com/michalvich/cucumber/selenium/example/GoogleSearchScenario.java
@@ -3,6 +3,7 @@ package com.michalvich.cucumber.selenium.example;
 import cucumber.annotation.en.Given;
 import cucumber.annotation.en.Then;
 import cucumber.annotation.en.When;
+import cucumber.runtime.PendingException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -15,20 +16,21 @@ public class GoogleSearchScenario {
 
 	private WebDriver driver = new HtmlUnitDriver();
 
-	@Given("^the page \\\"([^\\\"]*)\\\" is open$")
+	@Given("^the page is open \"([^\"]*)\"$")
 	public void the_page_is_open(String page) throws Throwable {
 		driver.get(page);
 	}
 
-	@When("^I search for \\\"([^\\\"]*)\\\"$")
+	@When("^I search for \"([^\"]*)\"$")
 	public void I_search_for(String search) throws Throwable {
 		WebElement element = driver.findElement(By.name("q"));
 		element.sendKeys(search);
 		element.submit();
 	}
 
-	@Then("^a browser title should contains \\\"([^\\\"]*)\\\"$")
+	@Then("^a browser title should contains \"([^\"]*)\"$")
 	public void a_browser_title_should_contains(String text) throws Throwable {
 		assertTrue(driver.getTitle().contains(text));
 	}
+
 }
--- src/test/resources/com/michalvich/cucumber/selenium/example/GoogleSearch.feature
+++ src/test/resources/com/michalvich/cucumber/selenium/example/GoogleSearch.feature
@@ -4,6 +4,6 @@ Feature: Search on Google
   So that I can see results
 
   Scenario: results are shown
-    Given the page "http://www.google.com" is open
+    Given the page is open "http://www.google.com"
     When I search for "Cucumber"
     Then a browser title should contains "Cucumber"
